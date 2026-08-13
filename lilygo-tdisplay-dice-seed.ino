d5d40be804feb90970c8e62dd9f2838f7fc3759b
Andreas Schjønhaug <andreas@schjonhaug.com>
2026-08-13 12:16:24 +0200

    Align dice roll labels

--- lilygo-tdisplay-dice-seed.ino
+++ lilygo-tdisplay-dice-seed.ino
@@ -80,11 +80,11 @@ void drawChooseLength() {
   canvas.setTextColor(TFT_GREEN, TFT_BLACK);
   canvas.drawString("1: 12 words", 5, 43, 4);
   canvas.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
-  canvas.drawString("50 dice rolls", 10, 70, 2);
+  canvas.drawString("50 dice rolls", 5, 70, 2);
   canvas.setTextColor(TFT_YELLOW, TFT_BLACK);
   canvas.drawString("2: 24 words", 5, 87, 4);
   canvas.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
-  canvas.drawString("99 dice rolls", 10, 114, 2);
+  canvas.drawString("99 dice rolls", 5, 114, 2);
 }
 
 void drawPreflightWarning() {
